5f7ec26778290e7b260ceb42d88cf2caa26fd2ea
Shubhi7031 <shubhi7031@gmail.com>
2025-07-31 12:48:32 +0530

    Add files via upload

--- Practice_3.py
+++ Practice_3.py
@@ -94,3 +94,70 @@ print(my_list)
 
 my_list.extend([1,2,3])
 print(my_list)
+
+
+my_list= [11, 64, 42, 28, 67, 10,81,80,89]
+
+my_list.insert(4,1000)
+print(my_list)
+my_list.sort()
+print(my_list)
+
+my_list.append({(2+3j),6})
+print(my_list)
+
+my_list.extend(["bangalore"])
+my_list.extend("system")
+my_list.extend({(2+3j),6})
+print(my_list)
+
+my_list.pop(2)
+my_list.pop(3)
+my_list.pop(0)
+
+
+
+x= ["abc","klm","bdc","zyc"]
+x.sort()
+print(x)
+x.sort(reverse= True)
+print(x)
+
+x=["Delhi", "Mumbai", "Bangalore","Chennai","Uttarakhand"]
+x.sort(reverse= True)
+
+print(x)
+
+x= [1,2,8, "course", "object"]
+
+print(x)
+
+
+x.reverse()
+print(x)
+
+# Shallow copy and deep copy
+
+x= [1,2,3,4,5]
+y=x
+y[3]=100000
+print(x)
+print(y)
+
+x= [3,8,9,11]
+y=x.copy()
+y[3]=198
+print(x)
+print(y)
+
+x= [3,3,8,8,9,10,11,3,3,3,3,4]
+a= x.count(3)
+print(a)
+
+x.pop(-1)
+print(x)
+x.pop()
+x.pop()
+x.pop()
+x.pop()
+print(x)
