08b148cd416357152b04c5378d86120e68f32a67
Shubhi7031 <shubhi7031@gmail.com>
2025-08-09 20:18:45 +0530

    Add files via upload

---
+++ Boolean_assignment.py
@@ -0,0 +1,122 @@
+# Question 1: Check if 25 is greater than 20 and less than 30
+print("Question 1: Check if 25 is greater than 20 and less than 30")
+# Your code here
+num=25
+if num>20 and num<30:
+    
+    print("True")
+else:
+    print("False")
+
+print("-"*100)
+
+# Question 2: Check if string "python" is equal to "Python" (case sensitive)
+print("\nQuestion 2: Check if string 'python' is equal to 'Python' (case sensitive)")
+# Your code here
+
+str1= "python"
+str2="Python"
+
+if str1 == str2:
+    print("string are same")
+else:
+    print("strings are not same")
+
+print("-"*100)
+
+# Question 3: Check if 15 is divisible by both 3 and 5
+print("\nQuestion 3: Check if 15 is divisible by both 3 and 5")
+# Your code here
+
+num=15
+
+if (num%3==0 and num%5==0):
+    print("15 is divisible by both 3 and 5")
+else:
+    print("15 is not divisible by 3 and 5")
+
+print("-"*100)
+
+# Question 4: Check if 7 is not equal to 8
+print("\nQuestion 4: Check if 7 is not equal to 8")
+# Your code here
+
+if 7==8:
+    print("equal")
+else:
+    print("not equal")
+
+print("-"*100)
+
+# # Question 5: Check if 100 is greater than 50 or less than 25
+print("\nQuestion 5: Check if 100 is greater than 50 or less than 25")
+# Your code here
+
+num=100
+
+if num>50 or num<25:
+    print("True")
+else:
+    print("False")
+
+
+print("-"*100)
+
+# Question 6: Check if 0 is falsy in Python
+print("\nQuestion 6: Check if 0 is falsy in Python")
+# Your code here
+
+if 0:
+    print("0 is truthy")
+else:
+    print("0 is falsy")
+
+print("-"*100)
+
+# Question 7: Check if empty string "" is falsy in Python
+print("\nQuestion 7: Check if empty string '' is falsy in Python")
+# Your code here
+
+if '':
+    print(" '' is truthy")
+else:
+    print(" '' is falsy")
+
+print("-"*100)
+
+# Question 8: Check if 42 is truthy in Python
+print("\nQuestion 8: Check if 42 is truthy in Python")
+# Your code here
+
+if 42:
+    print(" Truthy")
+else:
+    print("Falsy")
+
+print("-"*100)
+
+# Question 9: Check if 10 is between 5 and 15 (inclusive)
+print("\nQuestion 9: Check if 10 is between 5 and 15 (inclusive)")
+# Your code here
+
+num=10
+if 5<=num<=15:
+    print("num is in between 5 and 15 both inclusive")
+else:
+    print("num is not in between 5 and 15 both inclusive")
+
+
+print("-"*100)
+
+# Question 10: Check if 3.14 is greater than 3 and less than 4
+print("\nQuestion 10: Check if 3.14 is greater than 3 and less than 4")
+# Your code here 
+
+num=3.14
+
+if 3<num<4:
+    print("True")
+else:
+    print("False")
+
+print("-"*100)
