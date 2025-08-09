6e4f25f156ed52068326acb45aa61bb55258ee5b
Shubhi7031 <shubhi7031@gmail.com>
2025-08-09 21:53:19 +0530

    Add files via upload

---
+++ None_assignment.py
@@ -0,0 +1,125 @@
+# ASSIGNMENT QUESTIONS
+# ===================
+
+# Question 1: Create a variable and assign None to it
+print("Question 1: Create a variable and assign None to it")
+# Your code here
+
+var= None
+
+print(var)
+print("-"*100)
+
+
+# Question 2: Check if None equals None
+print("\nQuestion 2: Check if None equals None")
+# Your code here
+
+var= None
+
+if var == None:
+    print("equals")
+else:
+    print("Not equal")
+
+print("-"*100)
+
+# Question 3: Check if None is None
+print("\nQuestion 3: Check if None is None")
+# Your code here
+
+var= None
+
+if var is None:
+    print("None is None")
+else:
+    print("None is not none")
+
+print("-"*100)
+
+# Question 4: Check if 0 is None
+print("\nQuestion 4: Check if 0 is None")
+# Your code here
+
+Var= 0
+if Var is None:
+    print("yes")
+else:
+    print("No")
+
+print("-"*100)
+
+
+# Question 5: Check if empty string is None
+print("\nQuestion 5: Check if empty string is None")
+# Your code here
+
+str1= ""
+if str1 is None:
+    print("yes")
+else:
+    print("No")
+
+print("-"*100)
+
+# Question 6: Check if False is None
+print("\nQuestion 6: Check if False is None")
+# Your code here
+
+if False is None:
+    print("Yes")
+else:
+    print("No")
+
+
+print("-"*100)
+
+# Question 7: Check if empty list is None
+print("\nQuestion 7: Check if empty list is None")
+# Your code here
+
+list1=[]
+
+if list1 is None:
+    print("yes")
+else:
+    print("No")
+
+print("-"*100)
+
+# Question 8: Check if empty dictionary is None
+print("\nQuestion 8: Check if empty dictionary is None")
+# Your code here
+
+dict= {}
+if dict is None:
+    print("Yes")
+else:
+    print("No")
+
+print("-"*100)
+
+# Question 9: Check if empty tuple is None
+print("\nQuestion 9: Check if empty tuple is None")
+# Your code here
+
+tup= ()
+if tup is None:
+    print("Yes")
+else:
+    print("No")
+
+print("-"*100)
+
+# Question 10: Check if empty set is None
+print("\nQuestion 10: Check if empty set is None")
+# Your code here 
+
+set= ()
+
+if set is None:
+    print("Yes")
+else:
+    print("No")
+
+print("-"*100)
